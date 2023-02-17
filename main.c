fe23cb8dfe63719992a2932063fd3fccdcbc6f6e
Aleksandr.Kai <timofeev.aleksandr.mail@gmail.com>
2023-02-17 08:37:21 +0300

    optimizations

--- main.c
+++ main.c
@@ -1,53 +1,39 @@
 #include "conf.c"
 #include "sleep.h"
 
-// #define DEBUG
+#define DEBUG
 
 #define ENABLE_RADIO PORTD.2 = 1;
-#define DESABLE_RADIO PORTD.2 = 1;
+#define DESABLE_RADIO PORTD.2 = 0;
 #define ENABLE_CHARGING DDRC = 0x0E;PORTC.1 = 1;
 #define DESABLE_CHARGING DDRC = 0x0C;PORTC.1 = 0;
-#define DISCHARGE DDRC = 0x0E;delay_ms(10);DDRC = 0x0C;
-
-#ifdef DEBUG
-void send(int data)
-{
-   putchar(data >> 8);
-   putchar(data & 0xFF);
-   delay_ms(500);
-}
-#endif
-
-void discharge(int current){
-   int t = 19-(current-10)/55;
-   DDRC = 0x0E;
-   while(--t>0) delay_ms(1);
-   DDRC = 0x0C;
-}
+#define DISCHARGE DDRC = 0x0E;delay_ms(5);DDRC = 0x0C;
 
 void main(void)
 {
    int temp = 0;
    init();
 
+   DISCHARGE
+
    temp = read_adc(0);   
    
    #ifdef DEBUG
-   send(temp);
+   putchar(temp >> 8);
+   putchar(temp & 0xFF);
+   delay_ms(10);
    #endif
 
-   discharge(temp); // use current charge for calc discharge impulse
-
-   if (temp < 5){
+   if (temp < 10){
       ENABLE_CHARGING
       ENABLE_RADIO
-
       temp = ds18b20_temperature(0) * 10;
+
       putchar(0x88);
       putchar(temp >> 8);
       putchar(temp & 0xFF);
       
-      delay_ms(500);
+      delay_ms(20);
       DESABLE_RADIO
       DESABLE_CHARGING
    }
